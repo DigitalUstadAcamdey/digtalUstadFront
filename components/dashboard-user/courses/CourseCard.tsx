a0a8a3a276b4b27c858b64c942ee63ff45e80886
imadkasse <kasseimad81@gmail.com>
2025-09-21 21:16:48 +0100

    edit image cover

--- components/dashboard-user/courses/CourseCard.tsx
+++ components/dashboard-user/courses/CourseCard.tsx
@@ -25,12 +25,12 @@ const CourseCard = ({
 }: Props) => {
   return (
     <div className="my-1  drop-shadow-md border w-full border-gray-200 flex flex-col justify-between px-3 py-4 bg-white dark:bg-[#1a1c3d] dark:border-gray-700 rounded-xl transition-colors duration-300">
-      <div className="w-full aspect-[3/2] flex">
+      <div className="w-full aspect-[705/397] flex">
         <Image
           src={courseImg}
           alt="course1"
           loading="lazy"
-          className="rounded-lg w-full h-full object-contain bg-red-400"
+          className="rounded-lg w-full h-full object-contain "
         />
       </div>
 
